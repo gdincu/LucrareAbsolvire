112cc837b4df529b3a92f600ab3640cc07bffc9d
gdincu <dici0137@scs.ubbcluj.ro>
2020-07-09 22:16:03 +0300

    20200709-Added a controller

---
+++ EVCharge/API/Controllers/PointsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PointsController : ControllerBase
+    {
+
+        [HttpGet]
+        public string GetPoints()
+        {
+            return "GET";
+        }
+
+        [HttpGet("{id}")]
+        public string GetPoint(int id)
+        {
+            return "GET-ID";
+        }
+
+        [HttpPost]
+        public string CreatePoint()
+        {
+            return "POST";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdatePoint()
+        {
+            return "PUT";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeletePoint(int id)
+        {
+            return "DELETE";
+        }
+
+    }
+}
