51dff5cdce3fe585c707af014ad88bfde86537be
gdincu <dici0137@scs.ubbcluj.ro>
2020-07-12 12:53:33 +0300

    20200712-Added Swagger Documentation

--- EVCharge/API/Startup.cs
+++ EVCharge/API/Startup.cs
@@ -30,6 +30,8 @@ namespace Core
             services.AddScoped<IChargingPointRepository, ChargingPointRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(typeof(MappingProfiles));
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +43,18 @@ namespace Core
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //Serving the Swagger UI at the app's root
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
