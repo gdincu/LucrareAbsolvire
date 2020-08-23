839d54bf40de551e0c9607378a4c220f83963773
gdincu <dici0137@scs.ubbcluj.ro>
2020-08-24 00:23:53 +0300

    20200823 - Added client error handling

--- EVCharge/Client/src/app/app-routing.module.ts
+++ EVCharge/Client/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ChargingPointDetailsComponent } from './shop/charging-point-details/charging-point-details.component';
-import { ShopComponent } from './shop/shop.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'test-error', component: TestErrorComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: 'not-found', component: NotFoundComponent },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
